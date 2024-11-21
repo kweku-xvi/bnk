54516ea2fa1753cbdfc11004cc65189c718c4e6e
kweku-xvi <nksarps@gmail.com>
2024-11-21 16:21:32 +0000

    added urls for user registration, login and password reset

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path("signup", views.sign_up, name="sign_up"),
+    path("verify-user", views.verify_user, name="verify_user"),
+    path("login", views.login, name="login"),
+    path("password-reset", views.password_reset, name="password_reset"),
+    path("password-reset-confirm", views.password_reset_confirm, name="password_reset_confirm")
+]
